bd0b93835e45732b64e75056c79e871ea7ccdd9c
noskymunzen <munzenmayer@outlook.com>
2023-02-26 14:30:41 -0300

    fix: removes duplicated environment folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EnvironmentModule } from './environment/environment/environment.module';
+import { EnvironmentModule } from './environment/environment.module';
 import { ExtractorModule } from './extractor/extractor.module';
 
 @Module({
