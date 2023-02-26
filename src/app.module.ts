96610180a4750a29be1ab4eceb06b3846050f1db
noskymunzen <munzenmayer@outlook.com>
2023-02-26 14:14:31 -0300

    feat: adds .env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnvironmentModule } from './environment/environment/environment.module';
 import { ExtractorModule } from './extractor/extractor.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), ExtractorModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    EnvironmentModule,
+    ScheduleModule.forRoot(),
+    ExtractorModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
