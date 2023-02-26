35d6e46692947740123080700ba527a891db1e5c
noskymunzen <munzenmayer@outlook.com>
2023-02-26 14:28:15 -0300

    feat: implements pexels extractor

--- src/extractor/extractor.service.ts
+++ src/extractor/extractor.service.ts
@@ -2,6 +2,7 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { ImageAPIExtractor } from './extractor.interface';
+import { PexelsExtractor } from './providers/pexels/pexels.extractor';
 import { WallhavenExtractor } from './providers/wallhaven/wallhaven.extractor';
 
 @Injectable()
@@ -12,6 +13,9 @@ export class ExtractorService {
     this.extractors = [
       new WallhavenExtractor(this.httpService, {
         apiKey: process.env.WALLHAVEN_APIKEY,
+      }),
+      new PexelsExtractor(this.httpService, {
+        apiKey: process.env.PEXELS_APIKEY,
       }),
     ];
   }
