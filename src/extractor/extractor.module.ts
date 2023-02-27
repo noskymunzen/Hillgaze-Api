4b42ac0f5202c1a73845d7605631f763cae96163
noskymunzen <munzenmayer@outlook.com>
2023-02-27 13:52:08 -0300

    feat: injects picture module

--- src/extractor/extractor.module.ts
+++ src/extractor/extractor.module.ts
@@ -1,14 +1,10 @@
-import { HttpModule } from "@nestjs/axios";
-import { Module } from "@nestjs/common";
-import { ExtractorService } from "./extractor.service";
+import { HttpModule } from '@nestjs/axios';
+import { Module } from '@nestjs/common';
+import { PictureModule } from 'src/pictures/pictures.module';
+import { ExtractorService } from './extractor.service';
 
 @Module({
-  imports: [
-    HttpModule,
-  ],
-  providers: [
-    ExtractorService,
-  ]
+  imports: [HttpModule, PictureModule],
+  providers: [ExtractorService],
 })
-export class ExtractorModule {
-}
+export class ExtractorModule {}
