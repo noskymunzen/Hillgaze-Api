83bc616686656b0c7a5c95791fb029fbf42339b3
noskymunzen <munzenmayer@outlook.com>
2023-02-27 13:48:04 -0300

    feat: imports picture and mongoose modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvironmentModule } from './environment/environment.module';
 import { ExtractorModule } from './extractor/extractor.module';
+import { PictureModule } from './pictures/pictures.module';
 
 @Module({
   imports: [
@@ -12,6 +14,10 @@ import { ExtractorModule } from './extractor/extractor.module';
       envFilePath: '.env',
     }),
     EnvironmentModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      dbName: process.env.MONGODB_DB,
+    }),
+    PictureModule,
     ScheduleModule.forRoot(),
     ExtractorModule,
   ],
