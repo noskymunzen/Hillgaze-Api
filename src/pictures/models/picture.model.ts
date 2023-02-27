f90608d6e9f6122995320ebdbb6210c227ea3bf4
noskymunzen <munzenmayer@outlook.com>
2023-02-27 13:44:22 -0300

    feat: adds picture model

---
+++ src/pictures/models/picture.model.ts
@@ -0,0 +1,60 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import {
+  IsArray,
+  IsEnum,
+  IsMongoId,
+  IsNotEmpty,
+  IsObject,
+  IsOptional,
+  IsString,
+  IsUrl,
+} from 'class-validator';
+import { Document, Types } from 'mongoose';
+import { Provider } from 'src/extractor/extractor.interface';
+
+@Schema({ timestamps: { createdAt: true } })
+export default class Picture<T extends {} = {}> {
+  @Prop({ type: Types.ObjectId })
+  @IsMongoId()
+  _id?: Types.ObjectId;
+
+  @Prop({ type: String, required: true })
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @Prop({ type: String, requeried: true })
+  @IsString()
+  @IsNotEmpty()
+  @IsUrl()
+  url: string;
+
+  @Prop({ type: [String], required: false })
+  @IsArray()
+  @IsString({ each: true })
+  @IsNotEmpty({ each: true })
+  tags: string[];
+
+  @Prop({ enum: Provider, required: true })
+  @IsEnum(Provider)
+  providerName: `${Provider}`;
+
+  @Prop({ type: String, required: true })
+  @IsNotEmpty()
+  @IsString()
+  providerId: string;
+
+  @Prop({ type: String, requeried: true })
+  @IsString()
+  @IsNotEmpty()
+  @IsUrl()
+  providerURL: string;
+
+  @Prop({ type: Object })
+  @IsOptional()
+  @IsObject()
+  providerMeta?: T;
+}
+
+export const PictureSchema = SchemaFactory.createForClass(Picture);
+export type PictureDocument = Picture & Document;
